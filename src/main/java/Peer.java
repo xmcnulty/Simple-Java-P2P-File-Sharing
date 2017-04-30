bbbeadc97ca8424ec46b7054c05954b883a91804
jhenselUNH <jhensel@iol.unh.edu>
2017-04-30 11:37:12 -0400

    Moved peer stuff and added notification for adding a file to the peer pool.

---
+++ src/main/java/Peer.java
@@ -0,0 +1,17 @@
+
+
+import java.io.*;
+import java.net.*;
+/**
+ * Main class for creating the Peer GUI
+ * @author jeffrey
+ *
+ */
+
+public class Peer {
+	
+    public static void main(String[] args) throws IOException {
+		GUI gui = new GUI();
+		gui.GUI();
+    }
+}
