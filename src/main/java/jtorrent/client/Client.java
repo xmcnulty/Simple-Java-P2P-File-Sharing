6714cb5faa727a8cc00ad87e6530dfde2579a6a5
xmcnulty <x.mcnulty@gmail.com>
2017-04-30 23:46:30 -0400

    Some client implementation.

--- src/main/java/jtorrent/client/Client.java
+++ src/main/java/jtorrent/client/Client.java
@@ -3,7 +3,9 @@ package jtorrent.client;
 import jtorrent.common.JPeer;
 import jtorrent.common.JTorrent;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.nio.channels.SocketChannel;
 import java.util.UUID;
 
 /**
@@ -11,7 +13,7 @@ import java.util.UUID;
  *
  * Created by Xavier on 4/30/17.
  */
-public class Client {
+public class Client implements ClientConnectionHandler.PeerListener {
     private static final String BITTORRENT_ID_PREFIX = "-TO0042-";
 
     private InetAddress address;
@@ -19,11 +21,26 @@ public class Client {
 
     private JPeer self;
 
-    public Client(InetAddress address, int port, JTorrent torrent) {
+    private ClientConnectionHandler connectionHandler;
+
+    public Client(InetAddress address, int port, JTorrent torrent) throws IOException {
         this.address = address;
         this.torrent = torrent;
 
         String id = Client.BITTORRENT_ID_PREFIX + UUID.randomUUID()
                 .toString().split("-")[4];
+
+        connectionHandler = new ClientConnectionHandler(this.torrent, this.address, port, id);
+        connectionHandler.addListener(this);
+    }
+
+    @Override
+    public void handleNewPeerConnection(SocketChannel channel, byte[] peerId) {
+
+    }
+
+    @Override
+    public void handleFailedConnection(JPeer peer) {
+
     }
 }
